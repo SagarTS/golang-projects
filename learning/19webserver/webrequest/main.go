f03e7920683ac735cd8511fbf8b79292ee9e2461
sagarts <sagartshrestha@gmail.com>
2022-11-25 17:42:49 +0545

    API response

---
+++ learning/19webserver/webrequest/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main(){
+	fmt.Println("Web request")
+	// PerformGetRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
+}
+
+func PerformGetRequest(){
+	const myurl = "http://localhost:8000/get"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println("Content length is: ", response.ContentLength)
+
+	var responseString strings.Builder
+	content,_ := ioutil.ReadAll(response.Body)
+	byteCount,_ := responseString.Write(content)
+
+	fmt.Println(byteCount)
+	fmt.Println(responseString.String())
+
+	// fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest(){
+	const myurl = "http://localhost:8000/post"
+
+	//fake json payload
+	requestBody := strings.NewReader(`
+		{
+			"coursename":"Let's go with Golang",
+			"price":0,
+			"platform":"learnCodeOnline.in"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content,_ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest(){
+	const myurl = "http://localhost:8000/postform"
+
+	//formdata
+	
+	data := url.Values{}
+	data.Add("firstname","bruce")
+	data.Add("lastname","wayne")
+	data.Add("email","bw@batman.com")
+
+	response, err := http.PostForm(myurl, data)
+	if err != nil{
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+}
