e6a2f3be0a75348d85969ab2540d383c8b40d25b
sagarts <sagartshrestha@gmail.com>
2022-11-25 17:43:12 +0545

    API response

---
+++ learning/20json/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type course struct {
+	Name string
+	Price int
+	Platform string
+	Password string
+	Tags []string
+}
+
+func main(){
+	fmt.Println("Welcome to JSON video")
+}
