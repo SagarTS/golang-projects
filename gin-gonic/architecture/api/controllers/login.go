4c213d12edb00e721ba736befc8bc3b30a74f7ce
SagarTS <sagartshrestha@gmail.com>
2023-10-13 16:57:40 +0545

    feat: add validation message for users

--- gin-gonic/architecture/api/controllers/login.go
+++ gin-gonic/architecture/api/controllers/login.go
@@ -3,13 +3,27 @@ package controllers
 import (
 	"architecture/api/services"
 	"architecture/models"
-	"fmt"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
 
+type ApiError struct {
+    Field string
+    Msg   string
+}
+
+func msgForTag(tag string) string {
+    switch tag {
+    case "required":
+        return "This field is required"
+    case "email":
+        return "Invalid email"
+    }
+    return ""
+}
 
 func LoginUser(c *gin.Context){
 	validate := validator.New()
@@ -25,13 +39,18 @@ func LoginUser(c *gin.Context){
 	}
 
 	err := validate.Struct(body)
-
-	if(err != nil){
-		for _,e := range err.(validator.ValidationErrors){
-			fmt.Printf("Field: %s, Error: %s\n",e.Field(), e.Tag())	
+	if err != nil {
+		var ve validator.ValidationErrors
+		if errors.As(err, &ve) {
+			out := make([]ApiError, len(ve))
+			for i, fe := range ve {
+				out[i] = ApiError{fe.Field(), msgForTag(fe.Tag())}
+			}
+			c.JSON(http.StatusBadRequest, gin.H{"errors": out})
 		}
+		return
 	}
-
+	
 	isUserAlreadyRegistered := services.LoginUser(body.Email, body.Password)
 
 	if !(isUserAlreadyRegistered) {
